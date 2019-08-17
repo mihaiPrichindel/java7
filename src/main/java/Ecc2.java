205d8dd600982b0985295b424d1d1e10eee9a481
Mihai <mihai.prichindel@gmail.com>
2019-08-17 14:48:17 +0300

    e

---
+++ src/main/java/Ecc2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Ecc2 {
+    public static void main(String[] args) {
+        System.out.println("Introduceti datele ecuatie: a, b si c");
+        double a, b, c, x1, x2, delta, radicalDelta;
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" a = ");
+        a = sc.nextDouble();
+        sc = new Scanner(System.in);
+        System.out.print(" b = ");
+        b = sc.nextDouble();
+        System.out.print(" c = ");
+        sc = new Scanner(System.in);
+        c = sc.nextDouble();
+        System.out.println("Ecutia introdusa este " + a + (" * X^2 + " + b + " X + " + c + " = 0"));
+        if(a == 0) {
+            System.out.println("Datele introduse nu sunt specifice unei ecuatii de gradul 2 - \"a = 0\"");
+            if(b==0) {
+                System.out.println("Datele introduse nu sunt specifice nici unei ecuatii de gradul 1 - \"b = 0\"");
+                if(c!=0) {
+                    System.out.println("Datele introduse sunt total gresite: \"a = 0\", \"b = 0\" si \"c = " + c + "\" " +
+                            "este diferit de 0 - nonsens");
+                } else {
+                    System.out.println("Datele introduse nu sunt specifice unei ecuatii de gradul 2!");
+                }
+            } else {
+                System.out.println("Datele introduse sunt specifice unei ecuatii de gradul 1 cu solutia x=" + (-c/b));
+            }
+        } else if(b==0) {
+            System.out.println("Ecuatia are 2 solutii egale X1=X2 = " + (Math.sqrt(-c/a)));
+        } else {
+            delta = (b*b-4*a*c);
+            if (delta < 0) {
+                System.out.println("Ecuatia nu are solutii in domeniul numerelor reale");
+            } else if (delta == 0) {
+                System.out.println("Delta = 0 - cele 2 solutii sunt egale - X1=X2 = " + (-b /(2*a)));
+            } else {
+                radicalDelta = Math.sqrt(delta);
+                System.out.println("X1 = " + (-b + radicalDelta)/(2*a));
+                System.out.println("X2 = " + (-b - radicalDelta)/(2*a));
+            }
+        }
+    }
+}
---
+++ src/main/webapp/home/addClient.jsp
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My First HTML Page</title>
+
+</head>
+<body>
+
+<div align="center">
+    <h2>Login page</h2>
+</div>
+<form action="HelloServlet" method="GET">
+    <div align="center">
+        <table>
+            <tr>
+                <td>
+                    <label>Username</label>
+                </td>
+                <td>
+                    <input type="text" id="username" name="username"/>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <label>Password</label>
+                </td>
+                <td>
+                    <input type="password" id="password" name="password"/>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" onclick="submitForm()" value="Login">
+                </td>
+            </tr>
+
+        </table>
+        <!--
+        aici trebuie sa: faceti output si sa luati atributul de pe request (loginResult) de pe request
+        stiti ca in jsp poti scrie cod java deci poti sa evaluezi ce request ai:
+        if (loginResult == false) {
+            trebuie sa construiesti un mesaj: de Ex. pass wrong
+        }
+        -->
+        <%! String output(String input) {
+            return input;
+        }
+        %>
+
+        <table>
+            <tr>
+                <td>
+                    <p>If you don't have an account - <a href="pages/register.html">Register here</a></p>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+
+</form>
+
+</body>
+</html>
